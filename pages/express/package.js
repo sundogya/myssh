b1ae4dd451e81517ad0cf9a69471c83f0d147394
sundogya <sundogya@163.com>
2018-09-11 18:01:44 +0800

    express add

--- pages/express/package.js
+++ pages/express/package.js
@@ -2,6 +2,7 @@
 var config = getApp().config;
 var pageNum = 1;
 var comCodes = [];
+var goodsInfo = [{GoodsName:''}];
 var goods = [1]
 var data = {};
 var send = {
@@ -9,6 +10,10 @@ var send = {
     province: 0,
     city: 0,
     area: 0
+  },
+  info:{
+    name:'',
+    mobile:''
   },
   comDetail:{
     code:'SF',
@@ -35,6 +40,10 @@ var receive = {
     province: 0,
     city: 0,
     area: 0
+  },
+  info: {
+    name: '',
+    mobile: ''
   },
   cityCode: {
     province: '',
@@ -64,8 +73,38 @@ Page({
     comCodes:[],
     goods:goods
   },
+  getInfo:function(e){
+    var dataNow = e.currentTarget.dataset
+    if (dataNow.role == 'send'){
+      if(dataNow.name == 'detail'){
+        send.city.detail = e.detail.value
+      }else if(dataNow.name == 'mobile'){
+        send.info.mobile = e.detail.value
+      }else if(dataNow.name == 'name'){
+        send.info.name = e.detail.value
+      }
+    }else if(dataNow.role == 'receive'){
+      if (dataNow.name == 'detail') {
+        receive.city.detail = e.detail.value
+      } else if (dataNow.name == 'mobile') {
+        receive.info.mobile = e.detail.value
+      } else if (dataNow.name == 'name') {
+        receive.info.name = e.detail.value
+      }
+    }
+  },
   fillGoods:function(e){
-    console.log(e)
+    var dataNow = e.currentTarget.dataset
+    if (!goodsInfo[dataNow.index]){
+      goodsInfo[dataNow.index] = {}
+    }
+    if(dataNow.name == 'GoodsName'){
+      goodsInfo[dataNow.index].GoodsName = e.detail.value
+    } else if (dataNow.name == 'GoodsWeight'){
+      goodsInfo[dataNow.index].GoodsWeight = e.detail.value
+    }else if(dataNow.name == 'Goodsquantity'){
+      goodsInfo[dataNow.index].Goodsquantity = e.detail.value
+    }
   },
   hide:function(e){
     e.show.city = 0
@@ -226,10 +265,118 @@ Page({
     })
   },
 
-  expressSub:function(){
-    console.log(send)
-    console.log(receive)
-    console.log()
+  expressSubmit:function(){
+    var postData = {}
+    var pattern = /^1[3456789]\d{9}$/
+    var flag = 1
+    postData.uid = wx.getStorageSync('openId')
+    postData.comCode = send.comDetail.code
+    postData.comName = send.comDetail.name
+    postData.commodity = goodsInfo
+    postData.sender = {}
+    postData.receiver = {}
+    if (send.cityCode.province && send.cityCode.city && send.city.area !='请选择区'){
+      postData.sender.provinceName = send.city.province
+      postData.sender.provinceCode = send.cityCode.province
+      postData.sender.cityName = send.city.city
+      postData.sender.cityCode = send.cityCode.city
+      postData.sender.expAreaName = send.city.area
+    }else{
+      flag = 0
+      wx.showModal({
+        title: '城市选择错误',
+        content: '请正确选择寄件人地址，这样我们可以更加合理的安排快递人员与您联系',
+        showCancel: false,
+        confirmText: '我知道啦',
+        confirmColor: '#5cae1f',
+      })
+    }
+    if(flag){
+      if (send.info.name && pattern.test(send.info.mobile) && send.city.detail) {
+        postData.sender.name = send.info.name
+        postData.sender.mobile = send.info.mobile
+        postData.sender.areaDetail = send.city.detail
+      } else {
+        wx.showModal({
+          title: '未正确填写寄件人联系方式',
+          content: '请正确填写寄件人联系方式，这样我们的快递小哥才能正常联系到您哟',
+          showCancel: false,
+          confirmText: '我知道啦',
+          confirmColor: '#5cae1f',
+        })
+      }
+    }
+    if(flag){
+      if (receive.cityCode.province && receive.cityCode.city && receive.city.area != '请选择区'){
+        postData.receiver.provinceName = receive.city.province
+        postData.receiver.provinceCode = receive.cityCode.province
+        postData.receiver.cityName = receive.city.city
+        postData.receiver.cityCode = receive.cityCode.city
+        postData.receiver.expAreaName = receive.city.area
+      }else{
+        flag = 0
+        wx.showModal({
+          title: '城市选择错误',
+          content: '请正确选择收件人地址，这样对方才能正常收货哟',
+          showCancel: false,
+          confirmText: '我知道啦',
+          confirmColor: '#5cae1f',
+        })
+      }
+    }
+    if(flag){
+      if (receive.info.name && pattern.test(receive.info.mobile) && receive.city.detail) {
+        postData.receiver.name = receive.info.name
+        postData.receiver.mobile = receive.info.mobile
+        postData.receiver.areaDetail = receive.city.detail
+      } else {
+        console.log(receive)
+        flag = 0
+        wx.showModal({
+          title: '未正确填写收件人联系方式',
+          content: '请正确填写收件人联系方式，这样我们的快递小哥才能正常联系到您哟',
+          showCancel: false,
+          confirmText: '我知道啦',
+          confirmColor: '#5cae1f',
+        })
+      }
+    }
+    if(flag){
+      console.log(postData)
+      wx.request({
+        url: config.server +'/api/expressPage',
+        data: postData,
+        header: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        dataType: 'json',
+        responseType: 'text',
+        success: res => {
+          if(res.data.errorCode == -1){
+            wx.showModal({
+              title: '操作失败',
+              content: '当前下单人数较多，请稍后重试',
+              showCancel: false,
+              confirmText: '我知道啦',
+              confirmColor: '#5cae1f',
+            })
+          }else if(res.data.errorCode == 0){
+            wx.showModal({
+              title: '下单成功',
+              content: '您已下单成功，稍后会有工作人员与您联系，您的订单号为' + res.data.LogisticCode + '，快递公司名称为'+res.data.comName+',您可以在我的里面查看您的快递信息',
+              showCancel: false,
+              confirmText: '我知道啦',
+              confirmColor: '#5cae1f',
+              success:res=>{
+                wx.redirectTo({url: '/pages/index/index'})
+              }
+            })
+          }
+        }
+      })
+    }
+
   },
 
   /**
